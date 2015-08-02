46aa272834ad59101a409803a9a08b799f35d147
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2015-08-02 16:05:10 +0530

    Fix issue in updating status

--- lib/psd_utils/parse.rb
+++ lib/psd_utils/parse.rb
@@ -12,8 +12,8 @@
         psd_image.write(Rails.root.join('tmp', "#{upload_id}_psd_resized.png"))
         resized_psd_image = File.open(Rails.root.join('tmp', "#{upload_id}_psd_resized.png"))
         meta_data = generate_meta_data psd
+        upload.status = 1
         upload.psd_image = resized_psd_image
-        upload.status  = :completed
         upload.save!
         upload.designs.create!(meta_data: meta_data)
       end
