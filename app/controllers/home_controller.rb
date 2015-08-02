e54375e388b98f22e95846ff8449d4d882cbc1e8
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2015-08-02 11:35:00 +0530

    Redirect to home page after sign in; fix home page layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, :except => [:show, :index]
+  before_action :authenticate_user!
 
   def index
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+   before_action :authenticate_user!, :except => [:landing]
+
   def landing
      render(:layout => "layouts/landing")
   end
